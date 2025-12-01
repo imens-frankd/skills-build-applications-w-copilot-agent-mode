8cb83c3f5cced2ccc3e115d7d11f3d00ddfd456a
imens-frankd <112538384+imens-frankd@users.noreply.github.com>
2025-12-01 11:44:50 +0000

    Let's make the octofit tracker app look nice, pretty, and add some color

--- octofit-tracker/frontend/src/App.js
+++ octofit-tracker/frontend/src/App.js
@@ -1,4 +1,5 @@
 
+
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Activities from './components/Activities';
@@ -6,6 +7,7 @@ import Leaderboard from './components/Leaderboard';
 import Teams from './components/Teams';
 import Users from './components/Users';
 import Workouts from './components/Workouts';
+import logo from '../public/octofitapp-small.png';
 
 
 function App() {
@@ -13,7 +15,10 @@ function App() {
     <Router>
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
         <div className="container-fluid">
-          <Link className="navbar-brand fw-bold" to="/">Octofit Tracker</Link>
+          <Link className="navbar-brand fw-bold d-flex align-items-center" to="/">
+            <img src={logo} alt="Octofit Logo" className="octofit-logo me-2" />
+            Octofit Tracker
+          </Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
