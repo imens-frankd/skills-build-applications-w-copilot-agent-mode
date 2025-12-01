944d5f5902c238d477d0822f8c792c926213cf8c
imens-frankd <112538384+imens-frankd@users.noreply.github.com>
2025-12-01 11:55:22 +0000

    Update logo path in App component  to fix error

--- octofit-tracker/frontend/src/App.js
+++ octofit-tracker/frontend/src/App.js
@@ -7,7 +7,7 @@ import Leaderboard from './components/Leaderboard';
 import Teams from './components/Teams';
 import Users from './components/Users';
 import Workouts from './components/Workouts';
-import logo from '../public/octofitapp-small.png';
+
 
 
 function App() {
@@ -16,7 +16,7 @@ function App() {
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
         <div className="container-fluid">
           <Link className="navbar-brand fw-bold d-flex align-items-center" to="/">
-            <img src={logo} alt="Octofit Logo" className="octofit-logo me-2" />
+            <img src="/octofitapp-small.png" alt="Octofit Logo" className="octofit-logo me-2" />
             Octofit Tracker
           </Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
